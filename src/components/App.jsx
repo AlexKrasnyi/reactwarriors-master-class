b203bb58a6101f1c0041981ef6bff925aa7bf7bb
AlexKrasnyi <58141826+AlexKrasnyi@users.noreply.github.com>
2020-07-05 17:58:06 +0300

    05.07

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+function MovieItem(props) {
+  return (
+    <div>
+      <p>{props.title}</p>
+      <p>{props.rating}</p>
+    </div>
+  );
+}
+
 function App() {
-  return <div>Hello ReactWarriors!</div>;
+  return (
+    <div>
+      <MovieItem title="Title new" rating={9.8} />
+    </div>
+  );
 }
 
 export default App;
